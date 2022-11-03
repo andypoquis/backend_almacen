fe9a7a84db8ae93e5c479e86642b5878531335fc
andypoquis <andrespoquis@gmail.com>
2022-11-03 10:30:59 -0500

    a

---
+++ src/api/area-production/routes/area-production.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * area-production router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::area-production.area-production');
