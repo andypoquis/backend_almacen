960b3f6b94b0861f97ba6b8a9882efee08d3255f
andypoquis <andrespoquis@gmail.com>
2022-11-21 13:07:32 -0500

    cambios

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST', 'containers-us-west-136.railway.app'),
       port: env.int('DATABASE_PORT', 7518),
-      database: env('DATABASE_NAME', 'postgres'),
+      database: env('DATABASE_NAME', 'railway'),
       user: env('DATABASE_USERNAME', 'postgres'),
       password: env('DATABASE_PASSWORD', '7h536LIhxzU7Wrugj0JG'),
       ssl: env.bool('DATABASE_SSL', false),
