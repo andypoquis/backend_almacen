d53251553ffa6d23f305cfa1f066f2ab1038923b
andypoquis <andrespoquis@gmail.com>
2022-12-12 13:20:16 -0500

    Config database

--- config/database.js
+++ config/database.js
@@ -2,13 +2,29 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'containers-us-west-136.railway.app'),
-      port: env.int('DATABASE_PORT', 7518),
+      host: env('DATABASE_HOST', 'containers-us-west-25.railway.app'),
+      port: env.int('DATABASE_PORT', 7682 ),
       database: env('DATABASE_NAME', 'railway'),
       user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', '7h536LIhxzU7Wrugj0JG'),
+      password: env('DATABASE_PASSWORD', 'MY59kH3CKqGEODEqKBsR'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
 });
+ 
+
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'postgres',
+//     connection: {
+//       host: env('DATABASE_HOST', '127.0.0.1'),
+//       port: env.int('DATABASE_PORT', 5432),
+//       database: env('DATABASE_NAME', 'warehouse_db'),
+//       user: env('DATABASE_USERNAME', 'postgres'),
+//       password: env('DATABASE_PASSWORD', 'animes57xz'),
+//       ssl: env.bool('DATABASE_SSL', false),
+//     },
+//   },
+// });
  
